04173507db154c97198bf34bddccf0560de98848
Daewoong Kim <bigirane2@gmail.com>
2025-03-24 17:33:01 +0900

    Add files via upload

---
+++ qna_site/qa/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.question_list, name='question_list'),
+    path('queston/<int:pk>/', views.question_detail, name='question_detail'),
+    path('ask/', views.ask_question,name='ask_question'),
+    
+]
